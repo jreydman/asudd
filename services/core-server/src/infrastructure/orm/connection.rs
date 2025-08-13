6274e580f7dbc3abe683a1d2ed31a647b9d1de0a
jreydman <pikj.reyderman@gmail.com>
2025-08-13 17:30:54 +0300

    source

--- services/core-server/src/infrastructure/orm/connection.rs
+++ services/core-server/src/infrastructure/orm/connection.rs
@@ -4,6 +4,8 @@ use std::env;
 
 use diesel::{Connection, pg::PgConnection};
 
+// ===========================================================================
+
 pub struct DbConnection;
 
 // ===========================================================================
@@ -11,8 +13,8 @@ pub struct DbConnection;
 impl DbConnection {
     pub fn establish() -> Result<PgConnection, Box<dyn std::error::Error>> {
         let database_url = env::var("DATABASE_URL")?;
-        let conn = PgConnection::establish(&database_url)?;
-        Ok(conn)
+        let connection = PgConnection::establish(&database_url)?;
+        Ok(connection)
     }
 }
 
